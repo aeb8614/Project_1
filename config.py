621ba0a45ae45681c09855cdf55a212905788f46
[bjaikaran] <bobbyjaikaran@gwu.edu>
2018-04-19 16:05:15 -0400

    Updates to Google API call for improved code standards using PyLint

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
+"""
+Configuration file for Project 1 API keys
+"""
 #Google Web Services:
-
-g_web="AIzaSyCKha8eMHL2hgvClp8eklQwjcV6zPSyL1Q"
-
-
+G_WEB = "AIzaSyCKha8eMHL2hgvClp8eklQwjcV6zPSyL1Q"
 
 #Google Maps:
-
-g_maps="AIzaSyCfWflT4UE8SUT2EswhgZKFGh5HB0BL-NQ"
+G_MAPS = "AIzaSyCfWflT4UE8SUT2EswhgZKFGh5HB0BL-NQ"
