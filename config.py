3b19c352187442e9d123cf4012b1f01aa21d1dd7
[bjaikaran] <bobbyjaikaran@gwu.edu>
2018-04-19 16:32:50 -0400

    version 3

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 """
 Configuration file for Project 1 API keys
 """
-#Google Web Services:
+#Google Web and Places API Key
 G_WEB = "AIzaSyCKha8eMHL2hgvClp8eklQwjcV6zPSyL1Q"
 
-#Google Maps:
+#Google Maps API Key
 G_MAPS = "AIzaSyCfWflT4UE8SUT2EswhgZKFGh5HB0BL-NQ"
+
+#Brewery API key
+BEER_KEY = "c0323ad9e2ee5235d3f2d8c61989fe39"
