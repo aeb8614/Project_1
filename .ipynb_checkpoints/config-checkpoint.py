742c691eaaac73c73f21e7a9ddc028b3a7678cee
[bjaikaran] <bobbyjaikaran@gwu.edu>
2018-04-21 11:02:58 -0400

    Organizing data into folders for easier review --  your welcome TAs

--- .ipynb_checkpoints/config-checkpoint.py
+++ .ipynb_checkpoints/config-checkpoint.py
@@ -9,3 +9,11 @@ G_MAPS = "AIzaSyCfWflT4UE8SUT2EswhgZKFGh5HB0BL-NQ"
 
 #Brewery API key
 BEER_KEY = "c0323ad9e2ee5235d3f2d8c61989fe39"
+
+census_api_key = "d5b9e13a058d205c69e82f3d4eb11f386a73647a"
+
+beer_key = "c0323ad9e2ee5235d3f2d8c61989fe39"
+
+g_web = "AIzaSyCKha8eMHL2hgvClp8eklQwjcV6zPSyL1Q"
+
+Censu = "d5b9e13a058d205c69e82f3d4eb11f386a73647a"
